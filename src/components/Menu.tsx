e885a2a8e224f0b4ddbeadb0ca4313ec357bdc00
Kamat Prahlad <kamatprahlad43@gmail.com>
2024-06-07 15:32:33 +0530

    git init

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -12,6 +12,12 @@ const Menu = () => {
                 open && (
                     <div className="">
                         <Link href="/">HomePage</Link>
+                        <Link href="/">Shop</Link>
+                        <Link href="/">Deals</Link>
+                        <Link href="/">About</Link>
+                        <Link href="/">Contact</Link>
+                        <Link href="/">Logout</Link>
+                        <Link href="/">Cart(1)</Link>
                     </div>
                 )
             }
