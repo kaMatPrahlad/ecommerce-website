3bbed736ca62a3815dc88da08a989d313131ef5f
Kamat Prahlad <kamatprahlad43@gmail.com>
2024-06-08 16:07:14 +0530

    "Updated next.config.mjs to allow remote images from GitHub, and modified CartModel.tsx and Navbar.tsx components with new UI elements and styling."

--- src/components/CartModel.tsx
+++ src/components/CartModel.tsx
@@ -1,9 +1,39 @@
-"use client"
+"use client";
+
+import Image from "next/image";
 
 const CartModel = () => {
-    return (
-        <div>CartModel</div>
-    )
-}
+  const cartItems = true;
+  return (
+    <div className=" w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
+      {!cartItems ? (
+        <div>Cart is Empty</div>
+      ) : (
+        <div className="flex gap-4">
+          <Image
+            src="https://avatars.githubusercontent.com/u/162600820?v=4"
+            alt=""
+            width={72}
+            height={96}
+            className="object-cover rounded-md"
+          />
+          <div className="flex flex-col justify-between w-full">
+            {/* TOP */}
+            <div className="top"></div>
+            {/* TITLE */}
+            <div className="tittle">
+              <h3>Product name</h3>
+              <div className="">$49</div>
+            </div>
+            {/* DESC */}
+            <div className="desc">avilable</div>
+            {/* BOTTOM */}
+            {/* <div className="bottom"></div> */}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default CartModel
+export default CartModel;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
             {/* BIGGER SCREENS TAILWIND */}
             <div className="hidden md:flex items-center justify-between gap-8 h-full">
                 {/* LEFT */}
-                <div className="w-1/3 xl:w-1/2 ">
+                <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
                     <Link href='/' className="flex items-center gap-3">
                         <Image src="/logo.png" alt="" width={24} height={24} />
                         <div className="text-2xl tracking-wide">LAMA</div>
