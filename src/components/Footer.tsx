a5c0c36e0a58e39dd4d02f5e6e48eadbc9cc9c16
Kamat Prahlad <kamatprahlad43@gmail.com>
2024-06-15 13:27:01 +0530

    git commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,23 @@
+<Image src="/visa.png" alt="" width={40} height={40} />;
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 const Footer = () => {
   return (
     <div className="py-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 bg-gray-100 text-sm mt-24">
       {/* TOP */}
-      <div className="flex justify-between gap-24">
+      <div className="flex flex-col md:flex-row justify-between gap-24">
         {/* LEFT */}
-        <div className="lg:w-1/4 md:w-1/2 w-full flex flex-col gap-8">
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
           <Link href="/">
-            <div className="text-2xl tracking-wide">PRAHLAD</div>
+            <div className="text-2xl tracking-wide">LAMA</div>
           </Link>
           <p>
             306, Ashadep Apt, Ghandhi Chowk, Phoolpada road, Virar 401305,
             Mumbai
           </p>
           <span className="font-semibold">kamatprahlad43@gmail.com</span>
-          <span className="font-semibold">+9 733 7383 737</span>
+          <span className="font-semibold">+1 234 567 890</span>
           <div className="flex gap-6">
             <Image src="/facebook.png" alt="" width={16} height={16} />
             <Image src="/instagram.png" alt="" width={16} height={16} />
@@ -26,10 +26,40 @@ const Footer = () => {
             <Image src="/x.png" alt="" width={16} height={16} />
           </div>
         </div>
-      </div>
-
-      {/* CENTER */}
-      <div className="hidden lg:flex justify-between w-1/2">
+        {/* CENTER */}
+        <div className="hidden lg:flex justify-between w-1/2">
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">COMPANY</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">About Us</Link>
+              <Link href="">Careers</Link>
+              <Link href="">Affiliates</Link>
+              <Link href="">Blog</Link>
+              <Link href="">Contact Us</Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">SHOP</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">New Arrivals</Link>
+              <Link href="">Accessories</Link>
+              <Link href="">Men</Link>
+              <Link href="">Women</Link>
+              <Link href="">All Products</Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">HELP</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">Customer Service</Link>
+              <Link href="">My Account</Link>
+              <Link href="">Find a Store</Link>
+              <Link href="">Legal & Privacy</Link>
+              <Link href="">Gift Card</Link>
+            </div>
+          </div>
+        </div>
+        {/* RIGHT */}
         <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
           <h1 className="font-medium text-lg">SUBSCRIBE</h1>
           <p>
@@ -37,14 +67,37 @@ const Footer = () => {
             much more!
           </p>
           <div className="flex">
-            <input type="text" placeholder="Email address" className="p-4" />
+            <input
+              type="text"
+              placeholder="Email address"
+              className="p-4 w-3/4"
+            />
+            <button className="w-1/4 bg-lama text-white">JOIN</button>
+          </div>
+          <span className="font-semibold">Secure Payments</span>
+          <div className="flex justify-between gap-4">
+            <Image src="/discover.png" alt="" width={40} height={20} />
+            <Image src="/skrill.png" alt="" width={40} height={20} />
+            <Image src="/paypal.png" alt="" width={40} height={20} />
+            <Image src="/mastercard.png" alt="" width={40} height={20} />
+            <Image src="/visa.png" alt="" width={40} height={20} />
           </div>
         </div>
       </div>
-      {/* RIGHT */}
-
       {/* BOTTOM */}
-      <div className=""></div>
+      <div className="flex flex-col md:flex-row items-center justify-between gap-8 mt-16">
+        <div className="">© 2024 Prahlad Shop</div>
+        <div className="flex flex-col gap-8 md:flex-row">
+          <div className="">
+            <span className="text-gray-500 mr-4">Language</span>
+            <span className="font-medium">India | English</span>
+          </div>
+          <div className="">
+            <span className="text-gray-500 mr-4">Currency</span>
+            <span className="font-medium">$ USD</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
