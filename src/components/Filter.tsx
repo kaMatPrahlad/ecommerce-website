f21389a85bffebe3319e4ace18440d2d796c623e
Kamat Prahlad <kamatprahlad43@gmail.com>
2024-06-15 14:35:35 +0530

    git commit

---
+++ src/components/Filter.tsx
@@ -0,0 +1,17 @@
+const Filter = () => {
+  return (
+    <div className="mt-12 flex justify-between">
+      <div className="flex gap-6">
+        <select
+          name="type"
+          id=""
+          className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+        ></select>
+        <option value=""></option>
+      </div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Filter;
